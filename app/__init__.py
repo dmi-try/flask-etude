b0b11bbf5514ad7cb8c9d02a938a7408e445401a
Dmitry Pyzhov <lux.place@gmail.com>
2017-07-07 01:04:15 +0300

    Handler for days counter (#8)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
+import math
+from datetime import datetime as dt
 
 app = Flask(__name__)
 
@@ -12,6 +14,24 @@ def ping():
 @app.route("/ping_json")
 def ping_json():
     return jsonify({'ping': 'pong'})
+
+
+@app.route("/days_counter")
+def days_counter():
+    def ordinal(n):
+        return "%d%s" % (n, "tsnrhtdd"[
+            (math.floor(n / 10) % 10 != 1) * (n % 10 < 4) * n % 10::4
+        ])
+    start_date = request.args.get('since')
+    title = request.args.get('title')
+    total = int(request.args.get('needed'))
+    delta = (dt.today() - dt.strptime(start_date, '%Y-%m-%d')).days + 1
+    return """
+Today is the {days} day of {title}. {left} to go. {percent}% complete.
+    """.format(
+        days=ordinal(delta), title=title, left=total - delta,
+        percent="%.0f" % (100*delta/total)
+    )
 
 
 if __name__ == "__main__":
