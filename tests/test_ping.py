9564009be733559a704e4ee613c007ed129e4fb1
Dmitry Pyzhov <lux.place@gmail.com>
2017-07-10 11:20:32 +0300

    Move counter tests to the separate file

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -20,31 +20,3 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         result = json.loads(response.data)
         self.assertEqual(result['ping'], 'pong')
-
-
-class DayCounterTestCase(unittest.TestCase):
-    def test_counter_complete(self):
-        tester = app.test_client(self)
-        response = tester.get(
-            '/days_counter?since=2010-01-01&title=text&needed=1')
-        assert b'Congrat' in response.data
-
-    def test_counter_no_since(self):
-        tester = app.test_client(self)
-        response = tester.get('/days_counter?title=txt&needed=1')
-        assert b"Please provide 'since' parameter" in response.data
-
-    def test_counter_no_deadline(self):
-        tester = app.test_client(self)
-        response = tester.get('/days_counter?since=2017-01-01&title=txt')
-        assert b"Today is the" in response.data
-
-    def test_counter_no_title(self):
-        tester = app.test_client(self)
-        response = tester.get('/days_counter?since=2017-01-01&needed=1')
-        assert b"None" not in response.data
-
-    def test_counter_title(self):
-        tester = app.test_client(self)
-        response = tester.get('/days_counter?since=2017-01-01&title=thetitle')
-        assert b"thetitle" in response.data
